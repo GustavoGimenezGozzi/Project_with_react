53f68fd3d63fd57fc4765a2efaf1c8c9e87cd5f4
GustavoGimenezGozzi <gggustavo20@gmail.com>
2025-09-14 02:46:28 -0300

    Add files via upload

---
+++ Project_with_react/src/app.jsx
@@ -0,0 +1,117 @@
+/*
+import { SaudacaoClasse } from "./components/SaudacaoClasse";
+import SaudacaoFuncional from "./components/SaudacaoFuncional";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <h1>Comparativo de Componentes</h1>
+      <SaudacaoClasse />
+      <SaudacaoFuncional />
+    </main>
+  );
+}
+*/
+
+/*
+import BoasVindas from "./components/BoasVindas";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <h1>Props em Ação</h1>
+      <BoasVindas nome="Marina" curso="Frontend com React" />
+      <BoasVindas nome="João" curso="Mobile com React Native" />
+    </main>
+  );
+}
+*/
+
+/*
+import Contador from "./components/Contador";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <h1>useState Básico</h1>
+      <Contador />
+    </main>
+  );
+}
+*/
+
+/*
+import UsuarioInfo from "./components/UsuarioInfo";
+import ListaNumeros from "./components/ListaNumeros";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <h1>Estado com Objetos e Arrays</h1>
+      <UsuarioInfo />
+      <hr />
+      <ListaNumeros />
+    </main>
+  );
+}
+*/
+/*
+import ContadorSeguro from "./components/ContadorSeguro";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <h1>Atualizações Seguras de Estado</h1>
+      <ContadorSeguro />
+    </main>
+  );
+}
+*/
+
+/*
+import CicloDeVida from "./components/CicloDeVida";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <CicloDeVida />
+    </main>
+  );
+}
+*/
+
+/*
+import BotaoEvento from "./components/BotaoEvento";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <BotaoEvento />
+    </main>
+  );
+}
+*/
+
+/*
+
+import TextoDinamico from "./components/TextoDinamico";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <TextoDinamico />
+    </main>
+  );
+}
+*/
+/*
+import MensagemLogin from "./components/MensagemLogin";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <MensagemLogin />
+    </main>
+  );
+}
+*/
+
+import MensagemUsuario from "./components/MensagemUsuario.Jsx";
+export default function App() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: 16 }}>
+      <MensagemUsuario ok={false} nome="Carlos" />
+    </main>
+  );
+}
